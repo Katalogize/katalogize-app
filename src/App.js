4f7d840e08c206547f73ea194fe79f3d33e08f73
JoaoMoraesJr <joaojr1997@gmail.com>
2022-08-01 21:36:45 -0300

    Add ga4 events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './assets/img/logo.svg';
 import './App.scss';
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize("G-YGXLTYRGCV");
+  // ReactGA.send({hitType: "pageview"});
+  ReactGA.send({ hitType: "pageview", page: "/in-development", title:"In Development" });
+
+
+  function sendInfo () {
+    console.log("Sending click logo event");
+    ReactGA.event({
+      category: "Click",
+      action: "Click on logo",
+      label: "Logo"
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <span onClick={() => sendInfo()}><img src={logo} className="App-logo" alt="logo"/></span>
         <p>
           Application in development. Please come back later.
         </p>
