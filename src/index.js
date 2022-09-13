7503b5b64793444cb5a32db618ccc31886e208f8
JoaoMoraesJr <joaojr1997@gmail.com>
2022-09-13 03:04:22 -0300

    Refresh authentication token

--- src/index.js
+++ src/index.js
@@ -3,19 +3,99 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, ApolloLink, Observable } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from "@apollo/client/link/error";
+import { gql } from '@apollo/client';
+
+const REFRESH_TOKEN = gql`
+  mutation RefreshToken($refreshToken: String!) {
+    refreshToken (refreshToken: $refreshToken) {
+      accessToken,
+      refreshToken,
+      userId,
+      username,
+      email,
+      roles
+    }
+  }
+`;
 
 // DEV config
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:8080/graphql',
-  cache: new InMemoryCache(),
 });
 
-// PROD Config
-// const client = new ApolloClient({
+//PROD config
+// const httpLink = createHttpLink({
 //   uri: 'https://api.katalogize.com/graphql',
-//   cache: new InMemoryCache(),
 // });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('accessToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const errorLink = onError(
+  ({graphQLErrors, networkError, operation, forward}) => {
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        switch(err.extensions.classification) {
+          case 'UNAUTHORIZED':
+            //Refresh Token
+            return new Observable(observer => {
+            refreshAuthToken().then(() => {
+              const token = localStorage.getItem('accessToken');
+              operation.setContext(({ headers = {} }) => ({
+                headers: {
+                  ...headers,
+                  authorization: token ? `Bearer ${token}` : "",
+                }
+              }));
+              const subscriber = {
+                next: observer.next.bind(observer),
+                error: observer.error.bind(observer),
+                complete: observer.complete.bind(observer),
+              };
+              return forward(operation).subscribe(subscriber);
+            })});
+          default:
+            console.log("Default error");
+        }
+      }
+    }
+  }
+)
+
+function refreshAuthToken () {
+  const refreshToken = localStorage.getItem('refreshToken');
+  return client.mutate({
+    mutation: REFRESH_TOKEN,
+    variables: { refreshToken }
+  })
+  .then(response => {
+    localStorage.setItem("accessToken", response.data.refreshToken.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken.refreshToken);
+    return true;
+  })
+  .catch(error => {
+    console.log(error)
+    localStorage.clear();
+    //Redirect to login page
+    // window.location.reload();
+    return false;
+  })
+}
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
+  cache: new InMemoryCache()
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,63 @@
 import "./Home.scss";
-// import { GOOGLE_AUTH_URL } from "../constants/constants";
+import { useQuery, gql } from '@apollo/client';
+import Catalog from "../../components/catalog/Catalog";
+
+const USER_CATALOGS = gql`
+  query GetAllCatalogsByLoggedUser {
+    getAllCatalogsByLoggedUser{
+      name,
+      description,
+      user {
+        firstName,
+        lastName
+      }
+    }
+  }
+`;
+
+const PUBLIC_CATALOGS = gql`
+  query GetAllCatalogs {
+    getAllCatalogs {
+      name,
+      description,
+      user {
+        firstName,
+        lastName
+      }
+    }
+  }
+`;
+
+
+function UserCatalogs() {
+  const { loading, error, data } = useQuery(USER_CATALOGS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getAllCatalogsByLoggedUser.map(({ id, name, description }) => (
+    <Catalog key={id} catalogData={{name, description}}></Catalog>
+  ));
+}
+
+function PublicCatalogs() {
+  const { loading, error, data } = useQuery(PUBLIC_CATALOGS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getAllCatalogs.map(({ id, name, description }) => (
+    <Catalog key={id} catalogData={{name, description}}></Catalog>
+  ));
+}
 
 function Home() {
   return (
     <div>
       <h1>Your Katalogs</h1>
+      <UserCatalogs></UserCatalogs>
       <h1>Public Katalogs</h1>
+      <PublicCatalogs></PublicCatalogs>
     </div>
   );
 }
